95ea351a4d6425cfd1b87af1a3ee26616827f96d
Beyer, Sean <sbeyer@gbp.com>
2020-05-18 17:22:17 -0500

    Add project files.

---
+++ SulBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SulBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
