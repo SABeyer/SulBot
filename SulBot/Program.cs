57c15f25907fa00f02360d617b2c30d6ab0f5d99
Beyer, Sean <sbeyer@gbp.com>
2020-05-18 19:58:20 -0500

    Working on token storage

--- SulBot/Program.cs
+++ SulBot/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Security.Authentication.ExtendedProtection;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 
 namespace SulBot
 {
     class Program
     {
+        private readonly DiscordSocketClient _client;
+
+        private static IConfigurationRoot configuration;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public Program()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += LogAsync;
+        }
+
+        public async Task MainAsync()
+        {
+            if(Environment.GetEnvironmentVariable("token") == null)
+            {
+                Console.WriteLine("Please enter the bot token: ");
+                var tokenString = Console.ReadLine();
+                Environment.SetEnvironmentVariable("token", tokenString);
+            }
+                await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
+            await _client.StartAsync();
+
+            // Block the program until it is closed.
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+            return Task.CompletedTask;
+        }
+
+        // This is not the recommended way to write a bot - consider
+        // reading over the Commands Framework sample.
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            // The bot should never respond to itself.
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            if (message.Content == "!ping")
+                await message.Channel.SendMessageAsync("pong!");
         }
     }
 }
